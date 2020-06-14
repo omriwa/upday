534be5f03b5d6fcd8f0b850548c6f812e88fa4c6
omri wallach <omriwallach1992@gmail.com>
2020-06-14 19:44:37 +0200

    Updates

---
+++ precache-manifest.6a48f21e0a5cd3c3dfadd95faab68e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9125ec2dc5d853455ee2e3bdd0fc25",
+    "url": "/upday/index.html"
+  },
+  {
+    "revision": "72ba4ea16fa056ded2df",
+    "url": "/upday/static/css/main.70dbbadd.chunk.css"
+  },
+  {
+    "revision": "ddcb9ae9daf283fdfd98",
+    "url": "/upday/static/js/2.203a1c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/upday/static/js/2.203a1c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ba4ea16fa056ded2df",
+    "url": "/upday/static/js/main.22056e2e.chunk.js"
+  },
+  {
+    "revision": "251d9e0ffc2453146dac",
+    "url": "/upday/static/js/runtime-main.764f0388.js"
+  }
+]);
