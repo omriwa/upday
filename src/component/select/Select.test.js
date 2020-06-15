42cef0364f29ffd92f0ac0b495af33e4484ed9be
omri wallach <omriwallach1992@gmail.com>
2020-06-15 13:02:44 +0200

    add select testing

--- src/component/select/Select.test.js
+++ src/component/select/Select.test.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import {Select} from './Select';
 
-test('renders learn react link', () => {
+test('renders select', () => {
     const options = [1, 2];
-    const onSelectedOption = () => { };
-    const { findAllByText } = render(<Select
+    let selectedOption = options[0];
+    const onSelectedOption = e => console.log(e);
+    const {container} = render(<Select
         options={options}
-        selectedOption={options[0]}
+        selectedOption={selectedOption}
         onSelectedOption={onSelectedOption}
     />);
-
+    
+    container.querySelectorAll('option').forEach((option, index) => {
+        expect(option.value === options[index]);
+    });
+    expect(container.querySelector('select').value === options[0] + '').toBeTruthy();
 });
